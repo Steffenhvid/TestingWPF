c285475792a06c4b5bddd77b6d500d5e9a478b9b
Steffen Hvid Hansen <steffen@kbsoftware.dk>
2021-12-09 16:22:11 +0100

    Fixing wrong push

---
+++ TestingWPF/ViewModelLocator/ViewModelLocator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingWPF.ViewModels;
+
+namespace TestingWPF.ViewModelLocator
+{
+    public class ViewModelLocator
+    {
+        public MainViewModel mainViewModel => App.Current.Services.GetService<MainViewModel>();
+    }
+}
---
+++ TestingWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingWPF.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private string title;
+
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+
+        public MainViewModel()
+        {
+            SetTexts();
+        }
+
+        private void SetTexts()
+        {
+            Title = "Application Title";
+        }
+    }
+}
